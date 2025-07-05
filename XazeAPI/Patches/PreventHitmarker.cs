29c54f07ca68b48145cf87ed88d151b2945b1ad0
Xaze <eriknachricht@gmail.com>
2025-07-05 15:41:52 +0200

    Fixed IL Patch Exception in PreventHitmarker Patch

--- XazeAPI/Patches/PreventHitmarker.cs
+++ XazeAPI/Patches/PreventHitmarker.cs
@@ -15,32 +15,46 @@ using XazeAPI.API.Events;
 namespace XazeAPI.Patches
 {
     [HarmonyPatchCategory(APILoader.PatchGroup)]
+    [HarmonyPriority(800)]
     [HarmonyPatch(typeof(Hitmarker), nameof(Hitmarker.CheckHitmarkerPerms))]
     public class PreventHitmarker
     {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
-            // Remove the final two instructions: ldc.i4.1 + ret
-            newInstructions.RemoveAt(newInstructions.Count - 1); // ret
-            newInstructions.RemoveAt(newInstructions.Count - 1); // ldc.i4.1
+            // Remove the last two instructions (ldc.i4.1 + ret)
+            // But first check that last instructions are what you expect to avoid errors
+            int count = newInstructions.Count;
+            if (count < 2 
+                || newInstructions[count - 1].opcode != OpCodes.Ret 
+                || newInstructions[count - 2].opcode != OpCodes.Ldc_I4_1)
+            {
+                // Just yield original if unexpected
+                foreach (var instr in newInstructions)
+                    yield return instr;
+                ListPool<CodeInstruction>.Shared.Return(newInstructions);
+                yield break;
+            }
 
-            // Inject:
-            // return CustomPreventHitmarker(adh, victim);
+            newInstructions.RemoveAt(count - 1); // ret
+            newInstructions.RemoveAt(count - 2); // ldc.i4.1
+
+            // Inject your custom call and return
             newInstructions.AddRange(new[]
             {
-                new CodeInstruction(OpCodes.Ldarg_0), // adh
-                new CodeInstruction(OpCodes.Ldarg_1), // victim
+                new CodeInstruction(OpCodes.Ldarg_0), // Load adh
+                new CodeInstruction(OpCodes.Ldarg_1), // Load victim
                 new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PreventHitmarker), nameof(CustomPreventHitmarker))),
                 new CodeInstruction(OpCodes.Ret)
             });
 
-            foreach (var t in newInstructions)
-                yield return t;
+            foreach (var instr in newInstructions)
+                yield return instr;
 
             ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
+
 
         public static bool CustomPreventHitmarker(AttackerDamageHandler adh, ReferenceHub victim)
         {
